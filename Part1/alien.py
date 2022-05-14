84de841cdb8614fdedf21db12f53585320980620
Dongrui Lou <theodorelou90112@outlook.com>
2022-05-14 13:35:49 +0200

    14.05.2022

---
+++ Part1/alien.py
@@ -0,0 +1,37 @@
+alien_o = {"color":"green","points":5}
+
+print(alien_o['color'])
+print(alien_o['points'])
+
+alien_o['x_position'] = 0
+alien_o['y_position'] = 25
+
+print(alien_o)
+
+alien_1 = {}
+
+print(alien_1)
+alien_1['color'] = 'green'
+alien_1['points'] = 9
+
+print(alien_1)
+
+# 一行一行运行
+alien_3 = {'x_position': 0 , 'y_position': 25 , 'speed': 'medium'}
+print(f"Original position : {alien_3['x_position']}")
+
+# 向右移动外星人
+# 根据当前速度确定将外星人向右移动多远
+
+if alien_3['speed'] == 'slow':
+    x_increment = 1
+elif alien_3['speed'] == 'medium':
+    x_increment = 2
+else :
+    x_increment = 3
+
+# 新位置为旧位置加上移动距离
+
+alien_3['x_position'] = alien_3['x_position'] + x_increment
+
+print(f"new position:{alien_3['x_position']}")
