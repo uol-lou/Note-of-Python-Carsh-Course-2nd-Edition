e7305aaddfdb58b64e33f2ecc192d1889b199a5d
Dongrui Lou <theodorelou90112@outlook.com>
2022-08-16 00:57:50 +0200

    16.08.2022

--- Part2/alien/会考政治满分秘籍.py
+++ Part2/alien/会考政治满分秘籍.py
@@ -2,7 +2,7 @@ print("我是懒狗~")
 import matplotlib.pyplot as plt  # plt 用于显示图片
 import matplotlib.image as mpimg  # mpimg 用于读取图片
 
-lena = mpimg.imread('doge.jpg')
+lena = mpimg.imread('会考政治知识骨架.jpg')
 lena.shape
 
 plt.imshow(lena)  # 显示图片
