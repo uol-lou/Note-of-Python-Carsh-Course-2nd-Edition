a5406991b413c292a560c396255f3e2171a4eede
Dongrui Lou <theodorelou90112@outlook.com>
2022-08-16 00:30:13 +0200

    16.08.2022

--- Part2/alien/会考政治满分秘籍.py
+++ Part2/alien/会考政治满分秘籍.py
@@ -1,13 +1,10 @@
 print("我是懒狗~")
-import matplotlib.pyplot as plt # plt 用于显示图片
-import matplotlib.image as mpimg # mpimg 用于读取图片
-import numpy as np
+import matplotlib.pyplot as plt  # plt 用于显示图片
+import matplotlib.image as mpimg  # mpimg 用于读取图片
 
-lena = mpimg.imread('lena.png') # 读取和代码处于同一目录下的 lena.png
-# 此时 lena 就已经是一个 np.array 了，可以对它进行任意处理
-lena.shape #(512, 512, 3)
+lena = mpimg.imread('doge.jpg')
+lena.shape
 
-plt.imshow(lena) # 显示图片
-plt.axis('off') # 不显示坐标轴
+plt.imshow(lena)  # 显示图片
+plt.axis('off')  # 不显示坐标轴
 plt.show()
-复制代码
