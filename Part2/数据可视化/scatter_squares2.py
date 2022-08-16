2f395293b25bdd4e2447b9a44ebdfda67a68484b
Dongrui Lou <theodorelou90112@outlook.com>
2022-08-16 15:24:19 +0200

    16.08.2022

--- Part2/数据可视化/scatter_squares2.py
+++ Part2/数据可视化/scatter_squares2.py
@@ -2,3 +2,15 @@
 import matplotlib.pyplot as plt
 
 x_values = range(1, 1000)
+# 记住这个写法
+y_values = [x**2 for x in x_values]
+
+plt.style.use("seaborn")
+fig,ax = plt.subplots()
+
+ax.scatter(x_values,y_values,s =10)
+
+# 设定XY轴的取值范围
+ax.axis([0,1000,0,1100000])
+
+plt.show()
