155f17962167d0a2d8564571f3940514233c6ef1
Dongrui Lou <theodorelou90112@outlook.com>
2022-08-17 22:01:34 +0200

    16.08.2022

--- Part2/数据可视化/下载数据/sitka_highs.py
+++ Part2/数据可视化/下载数据/sitka_highs.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+import csv
+
+filename = 'dataexport_20220817T193734.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    head_row = next(reader)
+    print(head_row)
